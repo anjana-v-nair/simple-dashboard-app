6eae6721f53bf98e65b63d25634c1fd5f7b9f17a
Anjana Nair <anjuviswan93@gmail.com>
2020-02-09 01:57:45 +0530

    Link and Route demo

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 import DashboardPage from './pages/dashboard/dashboard.component'
 import IntakePage from './pages/intake/intake.component'
 
-import {  Route } from "react-router-dom";
+import {  Link, Route } from "react-router-dom";
 
 class App extends React.Component{
 render(){
   return(
     <div>
+      <ul>
+        <li><Link to='/'>Dashboard</Link></li>
+        <li><Link to='/intake'>Intake</Link></li>
+      </ul>
       <Route  exact path='/' component= {DashboardPage} />
       <Route  path='/intake' component= {IntakePage} />
     </div>
